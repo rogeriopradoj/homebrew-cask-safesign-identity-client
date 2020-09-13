4cad4ef04d84aec9858b9aacd00ea9f401cf7635
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2020-09-13 20:40:08 -0300

    Change download to zip instead of dmg file

--- Casks/safesign-identity-client.rb
+++ Casks/safesign-identity-client.rb
@@ -1,9 +1,9 @@
 cask "safesign-identity-client" do
   version "3.5.3.0"
-  sha256 "d9dd30f89b13e9dfc0fd286ba643d292e02e5db5df54908638dd0e1ec4f82f60"
+  sha256 "52717e367244256a78e1c70568ed7f1d74b0d404b59f2e9f05224b6326b8f97b"
 
   # github.com/rogeriopradoj/homebrew-cask-safesign-identity-client/ was verified as official when first introduced to the cask
-  url "https://github.com/rogeriopradoj/homebrew-cask-safesign-identity-client/raw/master/vendor/aet__safesign-ic__3.5.3.0/SafeSign-IC-Standard-MacOS-3.5.3.0-AET.000-x86_64.app.dmg"
+  url "https://github.com/rogeriopradoj/homebrew-cask-safesign-identity-client/raw/master/vendor/aet__safesign-ic__3.5.3.0/tokenadmin.app.zip"
   name "SafeSign Identity Client"
   homepage "https://www.aeteurope.com/our-solutions/safesign-identity-client/"
 
